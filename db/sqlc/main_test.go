a6817ae1adf4ca61738d617cd9d7179f3260120e
Wes Anderson <wesley.herb.anderson@gmail.com>
2022-05-22 16:58:27 -0400

    adding basic transfer endpoint

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,22 +7,24 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
+	"github.com/w-h-a/simplebank/util"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:mysecret@localhost:3500/simple_bank?sslmode=disable"
-)
-
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("failed to connect to the db", err)
 	}
+
 	testQueries = New(testDB)
+
 	os.Exit(m.Run())
 }
